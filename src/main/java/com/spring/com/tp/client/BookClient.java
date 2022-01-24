1f442e9a22cd60deea2e4ec9e1d9675b6f4562aa
AlessandroBarrionuevo <AlessandroBarrionuevo1@gmail.com>
2022-01-24 18:38:59 -0300

    ¨¨

--- src/main/java/com/spring/com/tp/client/BookClient.java
+++ src/main/java/com/spring/com/tp/client/BookClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Component
-public class BookClient {
+public class    BookClient {
     private RestTemplate restTemplate;
     @Value("${book.hostIsbn}")
     private String url;
@@ -21,7 +21,7 @@ public class BookClient {
     }
 
     public BookResponse getBookFromGoogleApi(String isbn){
-        log.info("Get de peliculas a la api https://www.googleapis.com para obtener datos del libro");
+        log.info("Get to Api Book: {}, buscando el isbn: {}", url, isbn);
         String urlAPegar = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn;
         return this.restTemplate.getForObject(urlAPegar, BookResponse.class);
     }
